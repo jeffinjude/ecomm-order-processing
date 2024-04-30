48cc57d2844ed20140ea140d9b0de5fd23538b74
jeffinjude <jeffinjude@gmail.com>
2024-04-30 22:04:44 +0530

    order processing service initial commit

---
+++ src/test/java/com/jeffinjude/orderprocessing/EcommOrderProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffinjude.orderprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommOrderProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
